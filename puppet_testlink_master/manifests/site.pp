64161c01bb6af774d2044e48e03212ac63b5f35d
totoanas <anas_moutaki@hotmail.com>
2017-01-18 16:52:38 +0100

    Update site.pp

--- puppet_testlink_master/manifests/site.pp
+++ puppet_testlink_master/manifests/site.pp
@@ -1,3 +1,3 @@
 $tarball_url        = 'http://downloads.sourceforge.net/project/testlink/TestLink%201.9/TestLink%201.9.3/testlink-1.9.3.tar.gz'
-$tarball_dir        = regsubst($tarball_url, '^.*?/(\d\.\d+).*$', '\1')
+$testlink_dir            = regsubst($tarball_url, '^.*?/(testlink-\d\.\d+\.\d+).*$', '\1')
 notice ($tarball_dir )
