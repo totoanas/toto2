f809466fdcbac1a989b261cbf0d25357c91a6f3f
totoanas <anas_moutaki@hotmail.com>
2017-01-19 10:25:19 +0100

    Update init.pp

--- puppet_testlink_master/manifests/init.pp
+++ puppet_testlink_master/manifests/init.pp
@@ -39,12 +39,55 @@
 #
 
 class testlink (
-  $admin_email,
+   $admin_email,
   $db_root_password,
   $doc_root       = $testlink::params::doc_root,
   $tarball_url    = $testlink::params::tarball_url,
   $package_ensure = 'latest',
   $max_memory     = '2048'
+  $tarball_url        = 'http://downloads.sourceforge.net/project/testlink/TestLink%201.9/TestLink%201.9.3/testlink-1.9.3.tar.gz'
+  $apache_daemon      = '/usr/sbin/apache2'
+  $installation_files = ['api.php',
+                         'api.php5',
+                         'docs',
+                         'install',
+                         'third_party',
+                         'upload_area',
+                         'logs',
+                         'locale',
+                         'lnl.php',
+                         'linkto.php',
+                         'lib',
+                         'gui',
+                         'custom',
+                         'cfg',
+                         'extensions',
+                         'img_auth.php',
+                         'img_auth.php5',
+                         'includes',
+                         'index.php',
+                         'index.php5',
+                         'languages',
+                         'load.php',
+                         'load.php5',
+                         'maintenance',
+                         'mw-config',
+                         'opensearch_desc.php',
+                         'opensearch_desc.php5',
+                         'profileinfo.php',
+                         'redirect.php',
+                         'redirect.php5',
+                         'redirect.phtml',
+                         'resources',
+                         'serialized',
+                         'skins',
+                         'StartProfiler.sample',
+                         'tests',
+                         'thumb_handler.php',
+                         'thumb_handler.php5',
+                         'thumb.php',
+                         'thumb.php5',
+                         'testlink.phtml']
   ) inherits testlink::params {
 
   #Class['testlink'] -> Testlink::Instance<| |>
@@ -54,7 +97,10 @@ class testlink (
   $tarball_name             = regsubst($tarball_url, '^.*?/(testlink-\d\.\d+.*tar\.gz)$', '\1')
   $testlink_dir            = regsubst($tarball_url, '^.*?/(testlink-\d\.\d+\.\d+).*$', '\1')
   $testlink_install_path   = "/var/www"
-  #$testlink_instance_install_path   = "/var/www"
+  notice($tarball_dir)
+  notice($tarball_name)
+  notice($testlink_dir)
+  notice($testlink_install_path)
   
   # Specify dependencies
   Class['mysql::server'] -> Class['testlink']
